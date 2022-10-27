706423d5f9f53c6934e241892013d5ae71dee504
IshTakkar21 <itakkar@asu.edu>
2022-10-27 11:20:29 -0700

    initial commit

---
+++ src/main/Urinals.java
@@ -0,0 +1,10 @@
+// project: Count number of usable empty urinals
+// author: Ish Takkar
+
+package main;
+
+public class Urinals {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
